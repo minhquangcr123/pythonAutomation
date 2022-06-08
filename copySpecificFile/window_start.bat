1cd97bf085cd597f611ff3c0afbfac62045ad563
Tran Minh Quang (MS/EDA12-XC) <quang.tranminh@vn.bosch.com>
2022-06-08 19:33:19 +0700

    TRAN MINH QUANG Update some new script

--- copySpecificFile/window_start.bat
+++ copySpecificFile/window_start.bat
@@ -3,25 +3,35 @@
 ECHO Start application - Tran Minh Quang 
 COLOR 
 
+setlocal enabledelayedexpansion
+
 @REM Running application
-@REM SET /p source_path="Enter your directory you want to copy list file: "
-@REM SET /p dest_path="Enter your destination path: "
 
-@REM IF DEFINED source_path (
-@REM     IF NOT EXIST source_path (
-@REM         SET source_path=D:\PROJECT\Intergrator\mono_radar_for_int\build
-@REM     )
-@REM ) ELSE (
-@REM     SET source_path=D:\PROJECT\Intergrator\mono_radar_learning\build
-@REM )
+:enterSource
+SET /p source_path="Enter your directory you want to copy list file: "
 
-@REM IF DEFINED source_path (
-@REM     IF NOT EXIST dest_path (
-@REM         SET dest_path=D:\Tool\PDV\PDV_Gen\Inputs
-@REM     )
-@REM ) ELSE (
-@REM     SET dest_path=D:\PROJECT\Competences\pythonAutomation\copySpecificFile\forCopyTo
-@REM )
+:enterDest
+SET /p dest_path="Enter your destination path: "
 
-@REM python copyFile.py %source_path% %dest_path%
-@REM IF %ERRORLEVEL%==0 start "" "%dest_path%"
+IF DEFINED source_path (
+    IF NOT EXIST source_path (
+        SET source_path=D:\PROJECT\Intergrator\mono_radar_for_int\build
+    )
+) ELSE (
+    SET source_path=D:\PROJECT\Intergrator\mono_radar_learning\build
+)
+
+IF DEFINED dest_path (
+    IF NOT EXIST dest_path (
+        SET dest_path=D:\Tool\PDV\PDV_Gen\Inputs
+    )
+) ELSE (
+    SET dest_path=D:\Tool\PDV\PDV_Gen\Inputs
+)
+
+:start
+python copyFile.py %source_path% %dest_path%
+IF %ERRORLEVEL%==0 start "" "%dest_path%"
+pause
+
+goto:enterDest
