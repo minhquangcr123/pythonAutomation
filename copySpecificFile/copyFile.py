2405b2ceacfcbc2e2ff63f6a98f6af8e42b0bc23
Tran Minh Quang (MS/EDA12-XC) <Quang.TranMinh@vn.bosch.com>
2022-07-18 19:24:19 +0700

    TRAN MINH QUANG Update

--- copySpecificFile/copyFile.py
+++ copySpecificFile/copyFile.py
@@ -3,10 +3,18 @@
 import os
 import sys
 import shutil
-import json
+import argparse
+from ConfigParser import ConfigParser
 
 # For color in command promt
 os.system("")
+
+config_file = r'copyFile.ini'
+list_file = []
+source_path = ''
+dest_path = ''
+include = []
+exclude = []
 
 class STATUS:
     SUCCESS=0
@@ -38,15 +46,24 @@ class CopyToDest:
     # folder:       The folder that you want to look up
     # dest_path:    Path that you copy the file wanted to
     # TBD...
-    def __init__ (self, list_file, dest_path):
+    def __init__ (self, list_file):
         self.list_file = list_file
-        self.src_path = ' '
-        self.dest_path = dest_path
+        self.src_path = ''
+        self.dest_path = ''
+        self.include = []
+        self.exclude = []
         self.log_file_have_copied = []
 
     def setSourcePath(self, folder):
         self.src_path = folder
 
+    def setDestPath(self, folder):
+        self.dest_path = folder
+
+    def setInclude(self, incl):
+        self.include = incl 
+    def setExclude(self, exc):
+        self.exclude = exc
 
     def printInfor(self):
         print('Display some information :')
@@ -61,7 +78,15 @@ class CopyToDest:
 
         # Folder copy to
         print('---------------- Destination Path: ')
-        print ('\t->' + self.dest_path + '\n')
+        print ('\t->' + self.dest_path)
+        
+        # Folder copy to
+        print('---------------- include: ')
+        print ('\t-> {} \n'.format(self.include))
+
+        # Folder copy to
+        print('---------------- exclude: ')
+        print ('\t-> {} \n'.format(self.exclude))
 
     def copyFile(self):
         print('Find the specific file and copy to the destination folder!')
@@ -77,29 +102,30 @@ class CopyToDest:
         #     else:
         #         if os.path.isdir(self.src_path):
         #             self.copyFile()
-        for root, dirs, files in os.walk(self.src_path):
+
+        for root, dirs, files in os.walk(self.src_path):    
             for fileName in files:
                 self.src_path = os.path.abspath(os.path.join(root, fileName))
-                if fileName in self.list_file:
-                    displayException('Have find out a file need to copy in the list - ' + self.src_path)
+                if ((fileName in self.list_file) and any(x in self.src_path for x in self.include) and all(ex not in self.src_path for ex in self.exclude)): # \
                     try:
                         shutil.copy(self.src_path, self.dest_path)
-                    except shutil.SameFileError:
+                        displayException('Have find out a file need to copy in the list - ' + self.src_path)
+                    except shutil.Error:
                         pass
                     self.log_file_have_copied.append(self.src_path)      
+                    # displayException('Can not find the file {}'.format(fileName))
 
-    def deleteFileCopy(self, folder):
+    def cleanDestPath(self):
         file_avoid = [__file__, "TBD.txt"]
-        try: # Make sure the folder exist
-            files = os.listdir(folder)
-            for file in os.listdir(folder):
-                if file in self.list_file:
-                    displayException("Delete {} from ".format( file ) + folder, STATUS.WARNING)
-                    file_path = os.path.join(folder, file)
+        try: # Make sure the self.dest_path exist
+            for files in os.listdir(self.dest_path):
+                if files in self.list_file:
+                    displayException("Delete from {}, file delete: ".format( self.dest_path ) + files, STATUS.WARNING)
+                    file_path = os.path.join(self.dest_path, files)
                     os.remove(file_path)
                 
         except:
-            displayException("Do not have the folder {}".format(folder))
+            displayException("Do not have the destination path - Some thing wrong".format(self.dest_path))
        
 
     def writeLogToFile(self, file_log):
@@ -107,43 +133,109 @@ class CopyToDest:
         displayException("Write log source path to file : {}".format(log_file_path))
         
         with open(log_file_path, 'w') as file:
-            for source_path in self.log_file_have_copied:
+            for source_path in self.log_file_have_copied: 
                 file.write(source_path + '\n')
 
     def printFileLog(self):
         for source_path in log_file_have_copied:
             print ('{} \n'.format(source_path))
 
+    
+def readConfig(config_file, s_item):
+    config = ConfigParser()
+    config.read(config_file)
+    key_item = config.items(s_item)
+    return key_item
 
+def updateConfig():
+    print("Parse argument pass to the python script!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type = str, help ='A required config path')
+    parser.add_argument('--source', type = str, help ='A required soruce path')
+    parser.add_argument('--dest', type = str, help ='A required dest path')
+    parser.add_argument('--include', nargs="+", help ='<word>  to include for finding')
+    parser.add_argument('--exclude', nargs="+", help ='<word>  to exclude for finding')
+    args = parser.parse_args()
+
+    # Update config, source ,dest
+    global config_file
+    global list_file
+    global source_path
+    global dest_path
+    global include
+    global exclude
+
+    config_file = args.config
+    if args.source:
+        source_path = args.source
+    if args.dest:
+        dest_path = args.dest
+    if args.include:
+        include  = args.include 
+    if args.exclude:
+        exclude = args.exclude
+    
+def runCopy(source, dest, list_file, include, exclude):
+    # Copy file, delete file, write log file
+    copyObj = CopyToDest(list_file) 
+    copyObj.setSourcePath(source)
+    copyObj.setDestPath(dest)
+    copyObj.setInclude(include)
+    copyObj.setExclude(exclude)
+    copyObj.printInfor()
+    copyObj.cleanDestPath()     
+        
+    copyObj.copyFile()
+    # copyObj.writeLogToFile('log_of_copy.txt')
+
+
+
+def main():  
+    # Parse list file
+    global config_file
+    global list_file
+    global source_path
+    global dest_path
+    global include
+    global exclude
+
+    if (len(sys.argv) > 3) and ('--config' in sys.argv):
+        print('Update config file!')
+        updateConfig()
+        for key,item in readConfig(config_file, 'list_file'):
+            list_file.append(item)
+
+        runCopy(source_path, dest_path, list_file, include, exclude)
+
+    elif (len(sys.argv) > 1) and ('--config' in sys.argv):
+        updateConfig()
+        print('Using default config file!')
+
+        for key,item in readConfig(config_file, 'list_file'):
+            list_file.append(item)
+
+        for key,item in readConfig(config_file, 'paths'):
+            if key == 'source':
+                source_path = item  
+            elif key == 'dest':
+                dest_path = item
+
+        for key,item in readConfig(config_file, 'include'):
+            include.append(item)
+        for key,item in readConfig(config_file, 'exclude'):
+            exclude.append(item)
+        
+        runCopy(source_path, dest_path, list_file, include, exclude)
+
+    else:
+        displayException("Please enter the --config <path_to_.ini> ", STATUS.ERROR)
+        updateConfig()
+        sys.exit(1)
+
+    sys.exit(0)
 
 if __name__ == '__main__'   :
-    if (len(sys.argv) > 2):
-        displayException("Argument(s) passed : {}".format(str(sys.argv)),STATUS.WARNING)
-        source_path = sys.argv[1] 
-        dest_path = sys.argv[2]
-        current_dir = r'.'
+    main()
 
-        list_file = [
-                        'log_of_copy.txt',
-                        'test.txt',
-                        'Dem_Cfg_DTC_DataStructures.c', 
-                        'Dem_Cfg_DtcId.h', 
-                        'Dem_Cfg_EventId.h',
-                        'Fee_EcucValuesPro.arxml',  
-                        'Fee_Report.txt',  
-                        'Fls_Report.txt',  
-                        'NvM_Report.txt',   
-                        'rba_DemBfm.properties', 
-                        'Dem.properties',  
-                    ]
-        # Copy file, delete file, write log file
-        copyObj = CopyToDest(list_file, dest_path) 
-        copyObj.setSourcePath(source_path)
-        copyObj.printInfor()
-        copyObj.deleteFileCopy(dest_path)
-        copyObj.copyFile()
-        # copyObj.writeLogToFile('log_of_copy.txt')
-        
-        
-    else:
-        displayException("Need enter <source_path> and <dest_path> for the script", STATUS.ERROR)
+    
+ 
--- copySpecificFile/window_start.bat
+++ copySpecificFile/window_start.bat
@@ -1,37 +1,16 @@
 @echo on
+@REM Call with argument:  %hardware%
 @REM Start application - Tran Minh Quang 
 ECHO Start application - Tran Minh Quang 
 COLOR 
 
+
 setlocal enabledelayedexpansion
 
 @REM Running application
-
-:enterSource
-SET /p source_path="Enter your directory you want to copy list file: "
-
-:enterDest
-SET /p dest_path="Enter your destination path: "
-
-IF DEFINED source_path (
-    IF NOT EXIST source_path (
-        SET source_path=D:\PROJECT\Intergrator\mono_radar_for_int\build
-    )
-) ELSE (
-    SET source_path=D:\PROJECT\Intergrator\mono_radar_learning\build
-)
-
-IF DEFINED dest_path (
-    IF NOT EXIST dest_path (
-        SET dest_path=D:\Tool\PDV\PDV_Gen\Inputs
-    )
-) ELSE (
-    SET dest_path=D:\Tool\PDV\PDV_Gen\Inputs
-)
+@REM
 
 :start
-python copyFile.py %source_path% %dest_path%
-IF %ERRORLEVEL%==0 start "" "%dest_path%"
-pause
+python copyFile.py -h
+python copyFile.py --config copyFile.ini
 
-goto:enterDest
